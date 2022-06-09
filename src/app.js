9148c13cc8edcfb04990d25edaf7f07f79b8346f
sahines5053 <sahines5053@gmail.com>
2022-06-09 05:29:14 -0500

    Added JS Day and Time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response);
   temperature = response.data.main.temp;
@@ -16,11 +34,13 @@ function displayTemperature(response) {
 
   let descriptionElement = document.querySelector("#day-description");
   descriptionElement.innerHTML = description;
+
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
+let city = "Austin";
 let apiKey = "c3b04aa14e86dfd85f0a148933f64476";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
