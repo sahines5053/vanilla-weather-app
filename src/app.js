afa72cdde6986733184b5452162f8c9ccac22c7e
sahines5053 <sahines5053@gmail.com>
2022-06-09 16:56:13 -0500

    Search engine taking city value

--- src/app.js
+++ src/app.js
@@ -65,6 +65,7 @@ function search(city) {
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
   console.log(cityInputElement.value);
 }
 
