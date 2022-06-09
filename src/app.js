bd54a0caaaab0e7533d8da610505f6c9ca07bf27
sahines5053 <sahines5053@gmail.com>
2022-06-09 04:57:06 -0500

    Added JS for temp, humidity, wind, description

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 function displayTemperature(response) {
+  console.log(response);
   temperature = response.data.main.temp;
+  humidity = response.data.main.humidity;
+  wind = response.data.wind.speed;
+  description = response.data.weather[0].main;
+
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = temperature;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(wind);
+
+  let descriptionElement = document.querySelector("#day-description");
+  descriptionElement.innerHTML = description;
 }
 
 let apiKey = "c3b04aa14e86dfd85f0a148933f64476";
