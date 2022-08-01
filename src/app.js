0b404e4c051422552f32c77be2904e1012377a61
sahines5053 <sahines5053@gmail.com>
2022-07-31 22:01:43 -0500

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -69,14 +69,12 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c3b04aa14e86dfd85f0a148933f64476";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
-  console.log(response);
   temperature = response.data.main.temp;
   humidity = response.data.main.humidity;
   wind = response.data.wind.speed;
@@ -123,7 +121,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 
 let form = document.querySelector("#search-form");
